2ca2fab214dc1df9794e069c9853f83341766c4d
unknown <std.feanor@gmail.com>
2013-04-19 17:13:08 +0300

    /api/register/encashment added to register EVENT_ENCASHMENT events.

--- webapi/register.py
+++ webapi/register.py
@@ -5,7 +5,7 @@ from u2py import transport_card
 from u2py import journey
 from u2py import purse
 from u2py import staff
-from u2py.events import EVENT_CONTRACT,EVENT_CONTRACT_ZALOG
+from u2py.events import EVENT_CONTRACT,EVENT_CONTRACT_ZALOG,EVENT_ENCASHMENT
 
 class WrongPasswordError(Exception):
  def __str__(self):
@@ -57,17 +57,36 @@ class register_cashier(APIHandler):
   })
 
  def POST(self,reader,password,answer={},**kw):
-   card = reader.scan()
-   answer['sn'] = card.sn.sn7()
-   transport_card.validate(card)
-   answer['aspp'] = str(card.aspp)
+  card = reader.scan()
+  answer['sn'] = card.sn.sn7()
+  transport_card.validate(card)
+  answer['aspp'] = str(card.aspp)
 
-   answer['staff'] = staff.read(card)
-   answer['personal'] = staff.read_personal_info(card)
+  answer['staff'] = staff.read(card)
+  answer['personal'] = staff.read_personal_info(card)
 
-   from u2py import config
-   if config.password != password: raise WrongPasswordError()
+  from u2py import config
+  if config.password != password: raise WrongPasswordError()
 
-   config.cash_card_sn = card.sn.sn8()
-   config.cash_card_aspp = str(card.aspp)
+  config.cash_card_sn = card.sn.sn8()
+  config.cash_card_aspp = str(card.aspp)
 
+class register_encashment(APIHandler):
+ url = '/api/register/encashment'
+
+ need_server = config.write_api_requires_server
+
+ def GET(self,answer={}):
+  answer.update({
+    'request': {
+        'reader' : 'число, индекс считывателя бесконтактных карточек',
+        'amount'     : 'общая выручка киоска на момент',
+        'tag_amount' : 'выручка киоска за жетоны на момент инкассации (входит в amount)',
+    },
+    'response': {
+        "error": 'Информация об ошибке, возникшей при выполнении вызова API.',
+    }
+  })
+
+ def POST(self,reader,amount,tag_amount,answer={},**kw):
+  EVENT_ENCASHMENT(Amount = amount, Value = tag_amount).save()
