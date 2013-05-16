f13a411e7ef1515ad924ab332e62d4f018e7fe4d
unknown <std.feanor@gmail.com>
2013-05-16 16:27:02 +0300

    Added config options for journey contract.
    + Logging config moved from internal config file to external.

--- u2py/config.py
+++ u2py/config.py
@@ -1,28 +1,16 @@
 ﻿import logging
 import logging.config
 
-logging.config.dictConfig({
- 'version': 1,    # Configuration schema in use; must be 1 for now
- 'formatters': {
-     'standard': {
-         'format': ('%(asctime)s '
-                    '%(levelname)-8s %(message)s')}},
- 'handlers': {'u2': { 'backupCount': 10,
-                       'class': 'logging.handlers.RotatingFileHandler',
-                       'filename': 'u2.log',
-                       'formatter': 'standard',
-                       'level': 'DEBUG',
-                       'maxBytes': 10000000 }
-             },
- # Specify properties of the root logger
- 'root': { 'level': 'DEBUG', 'handlers': ['u2'] },
-})
+lib_filename      = '../u2.dll'
+db_filename       = 'db.db3'
 
 reader_path       = [
     {'path': '\\\\.\\COM2', 'baud': 38400, 'impl':'asio' }
 ]
-lib_filename      = '../u2.dll'
-db_filename       = 'db.db3'
+
+max_journeys = 50
+journey_cost = 200
+
 term_full_cost    = 9500
 term_half_cost    = 4800
 hall_id           = 100
@@ -40,5 +28,5 @@ device_type       = 2
 password          = '123456'
 
 cash_card_sn      = 0
-cash_card_aspp    = '0000000000000000'
+cash_card_aspp    = '0' * 16
 
