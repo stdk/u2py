3f660d3b715549fafb1bfe9e2965566d7b649f2a
stdk <std.feanor@gmail.com>
2013-06-25 14:17:39 +0300

    Implemented /api/scan/notify
    It is now possible to sign for a notification about card
    appearing in reader field via aforementioned API.

---
+++ webapi/notify.py
@@ -0,0 +1,115 @@
+﻿from handlers_base import APIHandler
+import config
+
+from socket import AF_INET,SOCK_DGRAM
+from gevent import spawn,sleep,socket
+from multiprocessing.pool import ThreadPool
+from urlparse import urlparse
+from urllib2 import urlopen
+
+class UnsupportedProtocolError(Exception):
+ def __str__(self):
+  return 'This protocol is not supported'
+
+class IncorrectCallbackError(Exception):
+ def __str__(self):
+  return 'Calback does not conform to the correct format'
+
+class Notification(object):
+ def __init__(self,callback,sn = None):
+  self.sn = sn
+  self.parse_callback(callback,{
+    'tcp' : Notification.send_tcp,
+    'udp' : Notification.send_udp,
+    'http': Notification.send_http
+  })
+
+ def parse_callback(self,callback,senders):
+  try:
+   self.url = urlparse(callback)
+   self.sender = senders[self.url.scheme]
+  except IndexError: raise UnsupportedProtocolError()
+  except ValueError: raise IncorrectCallbackError()
+
+ def notify(self,pool,sn):
+  if self.sn != None and self.sn != sn: return
+  pool.apply_async(self.sender,args =(self.url,str(sn)))
+
+ @staticmethod
+ def send_tcp(url,sn):
+  #print 'send_tcp',url,sn
+  host,port = url.netloc.split(':')
+  s = socket.create_connection((host,int(port)))
+  s.send(str(sn))
+
+ @staticmethod
+ def send_udp(url,sn):
+  #print 'send_udp',url,sn
+  host,port = url.netloc.split(':')
+  s = socket.socket(AF_INET,SOCK_DGRAM)
+  s.sendto(str(sn),(host,int(port)))
+
+ @staticmethod
+ def send_http(url,sn):
+  #print 'send_http',url,sn
+  urlopen(url.geturl(),data = sn)
+
+class Notifier(object):
+ def __init__(self,poll_timeout = 1):
+  self.poll_timeout = poll_timeout
+  self.notifications = {}
+  self.pool = ThreadPool(5)
+
+  spawn(self.run)
+
+ def add_notification(self,reader_id,callback,**kw):
+  notification = Notification(callback,**kw)
+  if reader_id not in self.notifications:
+   self.notifications[reader_id] = {}
+  self.notifications[reader_id][callback] = notification
+
+ def remove_notification(self,reader_id,callback):
+  del self.notifications[reader_id][callback]
+
+ def run(self):
+  while True:
+   #print self.notifications
+   for reader_id,notifications in self.notifications.iteritems():
+    try:
+     with APIHandler.readers[reader_id].scan(lock = False) as card:
+      #print card
+      [n.notify(self.pool,card.sn.sn7()) for n in notifications.values()]
+    except IndexError:
+     del self.notifications[reader_id]
+    except Exception as e:
+     print e.__class__.__name__,e
+
+   sleep(self.poll_timeout)
+
+
+class scan_notify(APIHandler):
+ url = '/api/scan/notify'
+
+ need_server = config.read_api_requires_server
+
+ notifier = Notifier()
+
+ def GET(self,answer={}):
+  answer.update({
+   'request': {
+    'reader'  : 'число, индекс считывателя бесконтактных карточек',
+    'sn'      : 'опциональный параметр; cерийный номер требуемой бесконтактной карточки, число',
+    'action'  : 'строка, действие над нотификацией (add/remove)',
+    'callback': 'строка, адрес обратного вызова в виде URL',
+   },
+   'response': {
+    "error": 'Информация об ошибке, возникшей при выполнении вызова API.',
+   }
+  })
+
+ def POST(self, reader, action, callback, sn = None, answer = {}, **kw):
+  reader_id = self.readers.index(reader)
+  if action == 'add':
+   self.notifier.add_notification(reader_id, callback = callback, sn = sn)
+  if action == 'remove':
+   self.notifier.remove_notification(reader_id,callback = callback)
