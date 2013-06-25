c24ab0c4ba90da038ff32c10f4c22ee7b3f976f7
stdk <std.feanor@gmail.com>
2013-06-25 14:03:56 +0300

    Refactored identification function for use in other modules.

--- webapi/reader.py
+++ webapi/reader.py
@@ -2,6 +2,9 @@
 import config
 
 from u2py.interface import Reader
+from u2py.mfex import ReaderError
+
+__all__ = ['reader_detect','reader_save','reader_load']
 
 class reader_detect(APIHandler):
  url = '/api/reader/detect'
@@ -17,20 +20,20 @@ class reader_detect(APIHandler):
     }
   })
 
+ @staticmethod
+ def identify_port(port):
+  try:
+   reader = Reader(port,explicit_error = True)
+   version,sn = reader.version(),reader.sn()
+   return port,version,sn
+  except (ReaderError,IOError):
+   return None
+
  def POST(self, answer={}, **kw):
   from serial.tools import list_ports
-  from u2py.mfex import ReaderError
-
-  def identify_port(port):
-   try:
-    reader = Reader(port,explicit_error = True)
-    version,sn = reader.version(),reader.sn()
-    return port,version,sn
-   except (ReaderError,IOError):
-    return None
 
   [reader.close() for reader in APIHandler.readers]
-  readers = filter(lambda x:x,[identify_port('\\\\.\\' + com[0]) for com in list_ports.comports()])
+  readers = filter(lambda x:x,[self.identify_port('\\\\.\\' + com[0]) for com in list_ports.comports()])
   readers.sort(key = lambda (port,_1,_2): port)
   answer['readers'] = readers
   APIHandler.readers = [Reader(reader[0]) for reader in readers]
