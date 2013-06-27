fdfa76f8195b7a26acf6a30ed8895c22d14e56a1
stdk <std.feanor@gmail.com>
2013-06-27 15:31:39 +0300

    Notification test reworked

--- tests/manual_test_notify.py
+++ tests/manual_test_notify.py
@@ -5,17 +5,20 @@ or manually bringing card into actual reader field.
 '''
 from base import TestBase
 
-from gevent.server import StreamServer
+from gevent import Timeout
+from gevent.server import StreamServer,DatagramServer
+from gevent.wsgi import WSGIServer
 
-class TcpNotification(TestBase):
+class Notifications(TestBase):
  MAX_RESPONSE_COUNTER = 3
+ TIMEOUT = 3
 
  def setUp(self):
   self.host = '127.0.0.1'
   self.port = 4000
-  self.callback = 'tcp://%s:%i' % (self.host,self.port)
-  self.counter = 0
-  self.response = []
+
+ def init(self,protocol):
+  self.callback = '%s://%s:%i' % (protocol,self.host,self.port)
   self.sn = self.send_command('/api/scan',{ 'reader': 0 },check = False)['sn']
 
   self.send_command('/api/scan/notify', {
@@ -25,15 +28,6 @@ class TcpNotification(TestBase):
    'sn': self.sn
   })
 
-  self.server = StreamServer((self.host,self.port), self.handle_callback)
-  self.server.serve_forever()
-
- def handle_callback(self,socket,address):
-  if self.counter >= self.MAX_RESPONSE_COUNTER: self.server.stop()
-  print socket,address
-  self.response.append(socket.recv(128))
-  self.counter += 1
-
  def tearDown(self):
   self.send_command('/api/scan/notify', {
    'reader': 0,
@@ -42,7 +36,46 @@ class TcpNotification(TestBase):
   })
 
  def test_tcp_notification(self):
-  print self.response
+  self.init('tcp')
+
+  response = []
+  def handler(socket,address):
+   self.assertEquals(address[0],self.host)
+   response.append(socket.recv(128))
+
+  with Timeout(self.TIMEOUT,False):
+   StreamServer((self.host,self.port), handler).serve_forever()
+
+  self.assertTrue(all(str(self.sn) == sn for sn in response))
+
+ def test_udp_notification(self):
+  self.init('udp')
+
+  response = []
+  def handler(data,address):
+   self.assertEquals(address[0],self.host)
+   response.append(data)
+
+  with Timeout(self.TIMEOUT,False):
+   DatagramServer((self.host,self.port), handler).serve_forever()
+
+  self.assertGreater(len(response),0)
+  self.assertTrue(all(str(self.sn) == sn for sn in response))
+
+ def test_http_notification(self):
+  self.init('http')
+
+  response = []
+  def handler(environ, start_response):
+   response.append(environ['wsgi.input'].read())
+
+   start_response('200 OK', [('Content-type', 'text/plain')])
+   return ''
+
+  with Timeout(self.TIMEOUT,False):
+   WSGIServer((self.host, self.port), handler).serve_forever()
+
+  self.assertTrue(all(str(self.sn) == sn for sn in response))
 
 if __name__ == '__main__':
  import sys
