40a8f708eec685600d20cb26057c6c04aed6696c
stdk <std.feanor@gmail.com>
2013-07-09 17:20:56 +0300

    Internal configuration fix

--- webapi/config.py
+++ webapi/config.py
@@ -8,6 +8,7 @@ static_folder = os.path.join(webapi_folder,'static')
 templates_folder = os.path.join(webapi_folder,u'templates')
 
 notifier_poll_timeout = 0.5
+error_with_traceback = True
 
 read_api_requires_server = False
 write_api_requires_server = True
